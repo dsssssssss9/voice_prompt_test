fc75b15a65bd9bdf32dab2736d8ef5415149c9e2
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-24 20:51:38 +0000

    added volume variable & function

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
+def Set_Volume():
+    l = bytearray(7)
+    l.set_uint8(0, 0x7E)
+    l.set_uint8(1, 0x06)
+    l.set_uint8(2, 0x00)
+    l.set_uint8(3, 0x02)
+    l.set_uint8(4, 0x00)
+    l.set_uint8(5, Volume)
+    l.set_uint8(6, 0xEF)
+    serial.write_buffer(l)
+
 def on_button_pressed_a():
+    global Track_Num
+    Track_Num = smarttools.string_to_int(smarttools.dec2_hex(randint(1, 11)))
+    basic.show_string("" + str(Track_Num))
     Play_Track()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def Play_Track():
-    l = bytearray(7)
-    l.set_uint8(0, 0x7E)
-    l.set_uint8(1, 0x03)
-    l.set_uint8(2, 0x00)
-    l.set_uint8(3, 0x02)
-    l.set_uint8(4, 0x00)
-    l.set_uint8(5, Track_Num)
-    l.set_uint8(6, 0xEF)
-    serial.write_buffer(l)
+    m = bytearray(7)
+    m.set_uint8(0, 0x7E)
+    m.set_uint8(1, 0x03)
+    m.set_uint8(2, 0x00)
+    m.set_uint8(3, 0x02)
+    m.set_uint8(4, 0x00)
+    m.set_uint8(5, Track_Num)
+    m.set_uint8(6, 0xEF)
+    serial.write_buffer(m)
+Track_Num = 0
+Track_Num = 0
+Volume = smarttools.string_to_int(smarttools.dec2_hex(10))
 serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BAUD_RATE9600)
-Track_Num = smarttools.string_to_int(smarttools.dec2_hex(10))
-basic.show_icon(IconNames.YES)
-
-def on_forever():
-    pass
-basic.forever(on_forever)
+Set_Volume()
+basic.show_icon(IconNames.TARGET)
--- main.ts
+++ main.ts
@@ -1,21 +1,37 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+function Set_Volume () {
+    let l = control.createBuffer(7)
+l.setUint8(0, 0x7E)
+l.setUint8(1, 0x06)
+l.setUint8(2, 0x00)
+l.setUint8(3, 0x02)
+l.setUint8(4, 0x00)
+l.setUint8(5, Volume)
+l.setUint8(6, 0xEF)
+serial.writeBuffer(l)
+}
+input.onButtonPressed(Button.A, function () {
+    Track_Num = smarttools.stringToInt(smarttools.dec2Hex(randint(1, 10)))
+    basic.showString("" + Track_Num)
     Play_Track()
 })
-function Play_Track() {
-    let l = control.createBuffer(7)
-    l.setUint8(0, 0x7E)
-    l.setUint8(1, 0x03)
-    l.setUint8(2, 0x00)
-    l.setUint8(3, 0x02)
-    l.setUint8(4, 0x00)
-    l.setUint8(5, Track_Num)
-    l.setUint8(6, 0xEF)
-    serial.writeBuffer(l)
+function Play_Track () {
+    let m = control.createBuffer(7)
+m.setUint8(0, 0x7E)
+m.setUint8(1, 0x03)
+m.setUint8(2, 0x00)
+m.setUint8(3, 0x02)
+m.setUint8(4, 0x00)
+m.setUint8(5, Track_Num)
+m.setUint8(6, 0xEF)
+serial.writeBuffer(m)
 }
-
-serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate9600)
-let Track_Num = smarttools.stringToInt(smarttools.dec2Hex(10))
-basic.showIcon(IconNames.Yes)
-basic.forever(function on_forever() {
-    
-})
+let Track_Num = 0
+Track_Num = 0
+let Volume = smarttools.stringToInt(smarttools.dec2Hex(10))
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
+Set_Volume()
+basic.showIcon(IconNames.Square)
