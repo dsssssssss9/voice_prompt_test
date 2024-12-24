7a9e40b9fae44d35dfd085a51f226555a076858e
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-24 20:25:25 +0000

    reduce byte array to 7 not sure why it was 8 . In Principe it works

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ def on_button_pressed_a():
     Play_Track()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-
 def Play_Track():
-    
-    l = bytearray(8)
+    l = bytearray(7)
     l.set_uint8(0, 0x7E)
     l.set_uint8(1, 0x03)
     l.set_uint8(2, 0x00)
     l.set_uint8(3, 0x02)
     l.set_uint8(4, 0x00)
-    l.set_uint8(5, 0x02)
-    l.set_uint8(6, 5)
-    l.set_uint8(7, 0xEF)
+    l.set_uint8(5, 0x0A)
+    l.set_uint8(6, 0xEF)
+    
     serial.write_buffer(l)
-
-
-
 serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BAUD_RATE9600)
 Track_Num = 1
-basic.show_icon(IconNames.TORTOISE)
+basic.show_icon(IconNames.YES)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,25 +1,21 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     Play_Track()
 })
-function Play_Track () {
-    let l = control.createBuffer(8)
-l.setUint8(0, 0x7E)
-l.setUint8(1, 0x03)
-l.setUint8(2, 0x00)
-l.setUint8(3, 0x02)
-l.setUint8(4, 0x00)
-l.setUint8(5, 0x02)
-l.setUint8(6, 5)
-l.setUint8(7, 0xEF)
-serial.writeBuffer(l)
+function Play_Track() {
+    let l = control.createBuffer(7)
+    l.setUint8(0, 0x7E)
+    l.setUint8(1, 0x03)
+    l.setUint8(2, 0x00)
+    l.setUint8(3, 0x02)
+    l.setUint8(4, 0x00)
+    l.setUint8(5, 0x0A)
+    l.setUint8(6, 0xEF)
+    serial.writeBuffer(l)
 }
-serial.redirect(
-SerialPin.P0,
-SerialPin.P1,
-BaudRate.BaudRate9600
-)
+
+serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate9600)
 let Track_Num = 1
-basic.showIcon(IconNames.Tortoise)
-basic.forever(function () {
-	
+basic.showIcon(IconNames.Yes)
+basic.forever(function on_forever() {
+    
 })
