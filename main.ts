35ba92f9a37643dfed3d9e2ba4a06a4eac98401f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-24 20:08:11 +0000

    add bill sievers code

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    Play_Track()
+})
 function Play_Track () {
-    serial.writeString("[0x7E, 0x03, 0x00, 0x02, 0x00, Track_Num, 0xEF]")
+    let l = control.createBuffer(8)
+l.setUint8(0, 0x7E)
+l.setUint8(1, 0x03)
+l.setUint8(2, 0x00)
+l.setUint8(3, 0x02)
+l.setUint8(4, 0x00)
+l.setUint8(5, 0x02)
+l.setUint8(6, 5)
+l.setUint8(7, 0xEF)
+serial.writeBuffer(l)
 }
-function Set_Vol () {
-    serial.writeString("[0x7E, 0x06, 0x00, 0x02, 0x00, Vol, 0xEF]")
-}
-serial.setBaudRate(BaudRate.BaudRate9600)
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
 let Track_Num = 1
-let Vol = 20
+basic.showIcon(IconNames.Tortoise)
 basic.forever(function () {
 	
 })
