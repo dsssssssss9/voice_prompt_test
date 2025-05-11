42f5583a82c762b4b26b63ab2e3288b1acd469e7
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-05-11 21:58:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
 /**
+ * // i should like to point out that the original code on which my translation is based was written by Thomas Burzy - https://github.com/FireMarshmellow
+ * 
+ * // 
+ * 
+ * // He did all the haed work in translationg the not so easy to understand DFRobot manual to somethng much more readable - so he deserves the vast majority of the credit!!
+ * 
+ * // 
+ * 
+ * // He has a very good Youtube channel as well - https://www.youtube.com/mellow_labs
+ * 
+ * // 
+ * 
+ * // ******************************************************************************
+ * 
+ * // 
+ * 
+ * // ALSO i had some excellent help from Bill Sievers so he deserves a mention as well!!
+ * 
+ * *************************************************
+ * 
  * Speaker          MBit
  * 
  * -----------------------------
@@ -15,50 +35,6 @@
  * 
  * Also make sure to set Number_Of_Tracks variable in ONSTART block to correct value
  */
-/**
- * i should like to point out that the original code on which my translation is based was written by Thomas Burzy - https://github.com/FireMarshmellow
- * 
- * He did all the haed work in translationg the not so easy to understand DFRobot manual to somethng much more readable - so he deserves the vast majority of the credit!!
- * 
- * He has a very good Youtube channel as well - https://www.youtube.com/mellow_labs
- * 
- * ******************************************************************************
- * 
- * ALSO i had some excellent help from Bill Sievers so he deserves a mention as well!!
- * 
- * *****************************
- * 
- * // Speaker          MBit
- * 
- * // 
- * 
- * // 5V               3.3/5V
- * 
- * // 
- * 
- * // Gnd              GND
- * 
- * // 
- * 
- * // TX               P1 - 16
- * 
- * // 
- * 
- * // RX               p1 - 16
- * 
- * // 
- * 
- * // Ensure TX / RX pin numbers are correct in ONSTART Block
- */
-// i should like to point out that the original code on which my translation is based was written by Thomas Burzy - https://github.com/FireMarshmellow
-// 
-// He did all the haed work in translationg the not so easy to understand DFRobot manual to somethng much more readable - so he deserves the vast majority of the credit!!
-// 
-// He has a very good Youtube channel as well - https://www.youtube.com/mellow_labs
-// 
-// ******************************************************************************
-// 
-// ALSO i had some excellent help from Bill Sievers so he deserves a mention as well!!
 function Set_Volume () {
     let l = control.createBuffer(7)
 l.setUint8(0, 0x7E)
@@ -66,19 +42,10 @@ l.setUint8(1, 0x06)
 l.setUint8(2, 0x00)
 l.setUint8(3, 0x02)
 l.setUint8(4, 0x00)
-l.setUint8(5, 0x10)
+l.setUint8(5, Volume)
 l.setUint8(6, 0xEF)
 serial.writeBuffer(l)
 }
-input.onButtonPressed(Button.A, function () {
-    Track_Num += 1
-    if (Track_Num > Number_Of_Tracks) {
-        Track_Num = 1
-    }
-    Track_Num = smarttools.stringToInt(smarttools.dec2Hex(Track_Num))
-    basic.showString("" + (Track_Num))
-    Play_Track()
-})
 function Pause_Play () {
     let q = control.createBuffer(7)
 q.setUint8(0, 0x7E)
@@ -101,6 +68,16 @@ n.setUint8(5, 0x00)
 n.setUint8(6, 0xEF)
 serial.writeBuffer(n)
 }
+input.onButtonPressed(Button.A, function () {
+    Set_Volume()
+    Track_Num += 1
+    if (Track_Num > Number_Of_Tracks) {
+        Track_Num = 1
+    }
+    Track_Num = smarttools.stringToInt(smarttools.dec2Hex(Track_Num))
+    basic.showString("" + (Track_Num))
+    Play_Track()
+})
 function Next_Track () {
     let m = control.createBuffer(7)
 m.setUint8(0, 0x7E)
@@ -162,14 +139,14 @@ s.setUint8(5, 0x00)
 s.setUint8(6, 0xEF)
 serial.writeBuffer(s)
 }
-let Volume = 0
 let Number_Of_Tracks = 0
 let Track_Num = 0
+let Volume = 0
 // Make sure to set this to the maximum track number stored on the device you wish to use
-Number_Of_Tracks = 0
+Number_Of_Tracks = 10
 Track_Num = 0
+Volume = smarttools.stringToInt(smarttools.dec2Hex(16))
 basic.showIcon(IconNames.No)
-Volume = smarttools.stringToInt(smarttools.dec2Hex(10))
 serial.redirect(
 SerialPin.P16,
 SerialPin.P15,
